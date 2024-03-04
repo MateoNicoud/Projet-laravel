f44be35469dad608398b2fda3a0ae87dd0453478
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-03-04 13:28:03 +0100

    uiid fixed

--- database/migrations/2024_03_04_102505_create_products_table.php
+++ database/migrations/2024_03_04_102505_create_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
             $table->timestamps();
             $table->string('name');
             $table->string('description');

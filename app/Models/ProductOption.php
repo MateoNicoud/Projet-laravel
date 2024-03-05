84880194c2dfbe59278e8348a87493f00957ed74
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-03-05 14:33:38 +0100

    #51 - feature add relationship-between-product_options-and-url_img

--- app/Models/ProductOption.php
+++ app/Models/ProductOption.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class ProductOption extends Model
 {
     use HasFactory, HasUuids;
@@ -19,5 +22,8 @@ class ProductOption extends Model
         'weight',
         'stock'
     ];
-
+ public function urlimg(): BelongsTo
+ {
+        return $this->belongsTo(UrlImg::class);
+ }
 }

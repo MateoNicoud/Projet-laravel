70eada9615372eb4afc4fdab79f28e75ebf5ae6e
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-03-05 10:45:35 +0100

    #9 table order_lines is ready

--- app/Models/Order_line.php
+++ app/Models/Order_line.php
@@ -5,10 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order_line extends Model
 {
     use HasFactory, HasUuids;
+    public $timestamps = false;
+    protected $fillable = [
+      'option',
+      'reference',
+      'description',
+      'main_img',
+      'brand',
+      'price_ht',
+      'price_ttc',
+      'price_vat',
+      'weight',
+      'vat',
+      'value_promotion'
+    ];
     public function order(): HasOne
     {
         return $this->hasOne(Order::class);
---
+++ database/factories/Order_lineFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Order;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order_line>
+ */
+class Order_lineFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'order_id' => fake()->randomElement(Order::pluck('id')),
+            'option'=>fake()->word(),
+            'brand'=>fake()->word(),
+            'description'=>fake()->sentence(),
+            'main_img'=>fake()->imageUrl(),
+            'price_ht'=>fake()->randomFloat(1, 20, 30),
+            'price_ttc'=>fake()->randomFloat(1,20,30),
+            'price_vat'=>fake()->randomFloat(1,20,30),
+            'weight'=>fake()->randomFloat(1,0,100),
+            'reference'=>fake()->randomNumber(),
+            'value_promotion'=>fake()->numberBetween(0,100),
+            'quantity'=>fake()->randomDigitNotNull(),
+            'vat'=>fake()->randomElement([20, 10, 5.5, 2.1])
+        ];
+    }
+}

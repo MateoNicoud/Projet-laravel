b7d79c14713efe56e1d22395da6aee070f5650f9
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-03-06 13:46:22 +0100

    controller + view ok & productOptions + products + productCategory ok productImage NON OK

---
+++ resources/views/Product/show.blade.php
@@ -0,0 +1,8 @@
+<div>
+    coucou
+
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+</div>
+
+@php(dd($product,$productOptions,$productCategory,$productComments));
+

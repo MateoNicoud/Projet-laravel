3f32c2c4f52af7bdadb61657c671efaf59d95a7e
DUMAS <geremydumas@gmail.com>
2024-03-06 15:22:57 +0100

    #78 add header, navbar with responsive mode and modification categories tables for add slug column

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,104 @@
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
+        <div class="relative flex h-16 justify-between">
+            <div class="relative z-10 flex px-2 lg:px-0">
+                <div class="flex flex-shrink-0 items-center">
+                    <a href="/">
+                        <img class="block h-8 w-auto" src="{{ asset('assets/icons/logo_desktop.svg') }}" alt="Your Company">
+                    </a>
+                </div>
+            </div>
+            <div class="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
+                <div class="w-full sm:max-w-xs">
+                    <label for="search" class="sr-only">Search</label>
+                    <div class="relative">
+                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <input id="search" name="search" class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Search" type="search">
+                    </div>
+                </div>
+            </div>
+            <div class="relative z-10 flex items-center lg:hidden">
+                <!-- Mobile menu button -->
+                <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
+                    <span class="sr-only">Open menu</span>
+                    <!--
+                      Icon when menu is closed.
+
+                      Menu open: "hidden", Menu closed: "block"
+                    -->
+                    <svg id="icon_burger" class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    <!--
+                      Icon when menu is open.
+
+                      Menu open: "block", Menu closed: "hidden"
+                    -->
+                    <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+            <div class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
+                <button type="button" class="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <span class="sr-only">Identifie toi !</span>
+                    <button class="btn">S'identifier</button>
+                </button>
+
+                <!-- Profile dropdown -->
+                <div class="relative ml-4 flex-shrink-0">
+                    <div>
+                        <button type="button" class="flex bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                            <span class="sr-only">Ouvre ton panier</span>
+                            <img class="h-8 w-8 " src="{{asset('assets/icons/logo_cart.svg')}}" alt="">
+                        </button>
+                    </div>
+
+                    <!--
+                      Dropdown menu, show/hide based on menu state.
+
+                      Entering: "transition ease-out duration-100"
+                        From: "transform opacity-0 scale-95"
+                        To: "transform opacity-100 scale-100"
+                      Leaving: "transition ease-in duration-75"
+                        From: "transform opacity-100 scale-100"
+                        To: "transform opacity-0 scale-95"
+                    -->
+
+                </div>
+            </div>
+        </div>
+        <nav class="hidden lg:flex flex-wrap xl:flex-nowrap lg:space-x-8 lg:py-2" aria-label="Global">
+            @foreach($categories as $category)
+                <a href="/{{$category->slug}}" class="bg-gray-100 text-gray-900 inline-flex items-center rounded-md py-2 px-3 text-sm font-medium">{{$category->name}}</a>
+            @endforeach
+        </nav>
+    </div>
+
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <nav class="lg:hidden hidden" aria-label="Global" id="mobile-menu">
+        <div class="space-y-1 px-2 pb-3 pt-2">
+            <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
+            @foreach($categories as $category)
+                <a href="/{{$category->slug}}" class="text-gray-900 hover:bg-gray-50 hover:text-gray-900 block rounded-md py-2 px-3 text-base font-medium">{{$category->name}}</a>
+            @endforeach
+        </div>
+        <hr class="flex justify-center border-solid border-1 border-neutral-300 w-8/12 mx-auto rounded-full my-6">
+        <div class="mt-3 space-y-1 px-2">
+                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">Profil</a>
+                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">Paramètres</a>
+                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">Déconnexion</a>
+            </div>
+        <hr class="flex justify-center border-solid border-1 border-neutral-300 w-8/12 mx-auto rounded-full my-6">
+            <div class="mt-3 flex justify-center" >
+                <a class="w-20 bg-sky-300 rounded-lg" >
+                    <img class="p-3 mx-auto" src="{{asset('assets/icons/logo_cart.svg')}}">
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>

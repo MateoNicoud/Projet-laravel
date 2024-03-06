f2a2d6d82e97e39d9497985c60f138b62de27ebc
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-03-06 15:03:30 +0100

    added the mvc model for the homePage

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+<div class="flex justify-center">
+    <a href="http://last_man.local/">
+    <div class="card max-w-max bg-base-100 shadow-xl image-full">
+
+            <figure>
+                <img src="./assets/img/romain-gal-f5KkNedSydo-unsplash 2.webp" alt="liens vers l'article tendance"/>
+            </figure>
+
+        <div class="card-body">
+            <h2 class="card-title">SAC Gérémy 3000 en promo !</h2>
+            <p></p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">En savoir+</button>
+            </div>
+        </div>
+    </div>
+    </a>
+</div>
+<!-- catégorie les + populaires-->
+<div>
+    <!--COUTEAUX/HACHES -->
+    <a href="http://last_man.local/">
+        <div class="card max-w-max bg-base-100 shadow-none image-full">
+
+            <figure>
+                <img src="./assets/img/unsplash-ypxlkfj3ao.webp" alt="liens vers couteaux/haches"/>
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">COUTEAUX / HACHES</h2>
+                <p></p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-secondary">En savoir+</button>
+                </div>
+            </div>
+        </div>
+    </a>
+    <!-- TENTE -->
+    <a href="http://last_man.local/">
+        <div class="card max-w-max bg-base-100 shadow-none image-full">
+
+            <figure>
+                <img src="./assets/img/triston-dunn-gdelf4rmyby-unsplash-1.webp" alt="liens vers TENTE"/>
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">TENTE</h2>
+                <p></p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-secondary">En savoir+</button>
+                </div>
+            </div>
+        </div>
+    </a>
+    <!-- ORIENTATION -->
+    <a href="http://last_man.local/">
+        <div class="card max-w-max bg-base-100 shadow-none image-full">
+
+            <figure>
+                <img src="./assets/img/jamie-street-94hlr-qxo8-unsplash-1.webp" alt="liens vers ORIENTATION"/>
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">ORIENTATION</h2>
+            </div>
+        </div>
+    </a>
+    <!-- COUCHAGE -->
+    <a href="http://last_man.local/">
+        <div class="card max-w-max bg-base-100 shadow-none image-full">
+
+            <figure>
+                <img src="./assets/img/felix-m-dorn-hcti0k5e2iw-unsplash-1.webp" alt="liens vers ORIENTATION"/>
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">COUCHAGE</h2>
+            </div>
+        </div>
+    </a>
+    <!-- SAC ET TRANSPORT -->
+    <a href="http://last_man.local/">
+        <div class="card max-w-max bg-base-100 shadow-none image-full">
+
+            <figure>
+                <img src="./assets/img/romain-gal-f5kknedsydo-unsplash-3.webp" alt="liens vers SAC ET TRANSPORT"/>
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">SAC ET TRANSPORT</h2>
+            </div>
+        </div>
+    </a>
+</div>
+
+</body>
+</html>
+

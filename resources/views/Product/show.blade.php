c7b7fedb15880271f05f493e71ad28a55ee7f365
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-03-07 16:41:46 +0100

    je merde avec la view

--- resources/views/Product/show.blade.php
+++ resources/views/Product/show.blade.php
@@ -11,37 +11,96 @@
 
 {{--Categorie :{{$productOptions->name}}--}}
 <div>
-</div>
-<select name="option" id="selector">
-    <option >-- Selectionnez votre option --</option>
-@foreach($productOptions as $productOption)
-    <option value="{{ $productOption->option  }}" >
-        {{ $productOption->option }}
-    </option>
-@endforeach
-</select>
-<div>
-    Prix option : <span id="priceProduct"></span>
-</div>
-<div>
-{{--    Poids :{{$productOption->weight}}--}}
-        Poids option: <span id="weightProduct"></span>
-</div>
 
-<div>
-    Name :{{$product->name}}
-</div>
-<div>
-    Marque :{{$product->brand}}
-</div>
+ <div>
 
 
-<div>
-    description :{{$product->description}}
+
+ </div>
+
+
 </div>
-<div>
-    reference :{{$product->reference}}
+ <div class="flex flex-wrap p-3">
+<div class="flex-auto w-64">
+
+    <div class="h-96 carousel carousel-vertical rounded">
+        <div class="carousel-item h-full">
+            <img src="{{$productImg->main_img}}" class="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="{{$productImg->front_img}}" class="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="{{$productImg->left_img}}" class="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="{{$productImg->right_img}}" class="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="{{$productImg->back_img}}" class="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+    </div>
 </div>
+
+     <div class="flex-auto w-32">
+         <div>
+             Name :{{$product->name}}
+         </div>
+         <div>
+             Marque :{{$product->brand}}
+         </div>
+         <div>
+             reference :{{$product->reference}}
+         </div>
+         @foreach($productOptions as $productOption)
+         <fieldset name="option" id="selector" class="mt-4">
+             <legend class="sr-only">sélectionner votre option</legend>
+             <div class="grid grid-cols-3 gap-2 sm:grid-cols-8 lg:grid-cols-4">
+                 <!-- Active: "ring-2 ring-indigo-500" -->
+                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-not-allowed bg-gray-50 text-gray-200">
+                     <input type="radio" name="option" value="{{ $productOption->option  }}"  class="sr-only" aria-labelledby="size-choice-0-label">
+                     <span id="selector">{{ $productOption->option  }}</span>
+                     <span aria-hidden="true" class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">
+                    <svg class="absolute inset-0 h-full w-full stroke-2 text-gray-200" viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">
+                      <line x1="0" y1="100" x2="100" y2="0" vector-effect="non-scaling-stroke" />
+                    </svg>
+                  </span>
+                 </label>
+                 <!-- Active: "ring-2 ring-indigo-500" -->
+                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
+                     <input type="radio" name="size-choice" value="2XL" class="sr-only" aria-labelledby="size-choice-6-label">
+                     <span id="size-choice-6-label">{{ $productOption->option  }}</span>
+                     <!--
+                       Active: "border", Not Active: "border-2"
+                       Checked: "border-indigo-500", Not Checked: "border-transparent"
+                     -->
+                     <span class="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
+                 </label>
+
+             </div>
+         </fieldset>            @endforeach
+     </div>
+         <select name="option" id="selector">
+            <option >-- Selectionnez votre option --</option>
+        @foreach($productOptions as $productOption)
+                <option value="{{ $productOption->option  }}" >
+                    {{ $productOption->option }}
+                </option>
+            @endforeach
+        </select>
+        <div>
+            Prix option : <span id="priceProduct"></span>
+        </div>
+        <div>
+            {{--    Poids :{{$productOption->weight}}--}}
+            Poids option: <span id="weightProduct"></span>
+        </div>
+
+        description :{{$product->description}}
+    </div>
+
+ </div>
+
 <div>
     image principale :{{$productImg->main_img}}
 </div>

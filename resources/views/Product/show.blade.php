def222fb476613ca0fd00fd71374137ac77041f7
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-03-08 09:24:15 +0100

    on nettoie le code mort

--- resources/views/Product/show.blade.php
+++ resources/views/Product/show.blade.php
@@ -2,21 +2,9 @@
 @section('content')
     @include('partials.header')
 
-
-
-
     <div>
-        catalogue/catégorie/{{$product->slug}}
+        catalogue/{{$productCategory->name}}/{{$product->slug}}
     </div>
-
-    {{--Categorie :{{$productOptions->name}}--}}
-    <div>
-
-        <div>
-
-
-        </div>
-
 
     </div>
     <div class="flex flex-wrap p-3">
@@ -51,35 +39,6 @@
             <div>
                 reference :{{$product->reference}}
             </div>
-            {{--         @foreach($productOptions as $productOption)--}}
-            {{--         <fieldset name="option" id="selector" class="mt-4">--}}
-            {{--             <legend class="sr-only">sélectionner votre option</legend>--}}
-            {{--             <div class="grid grid-cols-3 gap-2 sm:grid-cols-8 lg:grid-cols-4">--}}
-            {{--                 <!-- Active: "ring-2 ring-indigo-500" -->--}}
-            {{--                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-not-allowed bg-gray-50 text-gray-200">--}}
-            {{--                     <input type="radio" name="option" value="{{ $productOption->option  }}"  class="sr-only" aria-labelledby="size-choice-0-label">--}}
-            {{--                     <span id="selector">{{ $productOption->option  }}</span>--}}
-            {{--                     <span aria-hidden="true" class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">--}}
-            {{--                    <svg class="absolute inset-0 h-full w-full stroke-2 text-gray-200" viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">--}}
-            {{--                      <line x1="0" y1="100" x2="100" y2="0" vector-effect="non-scaling-stroke" />--}}
-            {{--                    </svg>--}}
-            {{--                  </span>--}}
-            {{--                 </label>--}}
-            {{--                 <!-- Active: "ring-2 ring-indigo-500" -->--}}
-            {{--                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">--}}
-            {{--                     <input type="radio" name="size-choice" value="2XL" class="sr-only" aria-labelledby="size-choice-6-label">--}}
-            {{--                     <span id="size-choice-6-label">{{ $productOption->option  }}</span>--}}
-            {{--                     <!----}}
-            {{--                       Active: "border", Not Active: "border-2"--}}
-            {{--                       Checked: "border-indigo-500", Not Checked: "border-transparent"--}}
-            {{--                     -->--}}
-            {{--                     <span class="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>--}}
-            {{--                 </label>--}}
-
-            {{--             </div>--}}
-            {{--         </fieldset>            @endforeach--}}
-
-
             <div>
                 <select name="option" id="selector">
                     <option>-- Selectionnez votre option --</option>
@@ -93,7 +52,7 @@
                     Prix option : <span id="priceProduct"></span>
                 </div>
                 <div>
-                    {{--    Poids :{{$productOption->weight}}--}}
+
                     Poids option: <span id="weightProduct"></span>
                 </div>
 
@@ -111,21 +70,6 @@
             </div>
         </div>
 
-        {{--    <div>--}}
-        {{--    image principale :{{$productImg->main_img}}--}}
-        {{--</div>--}}
-        {{--    <div>--}}
-        {{--    image arrière :{{$productImg->back_img}}--}}
-        {{--</div>--}}
-        {{--    <div>--}}
-        {{--    image gauche :{{$productImg->left_img}}--}}
-        {{--</div>--}}
-        {{--    <div>--}}
-        {{--    image droite :{{$productImg->right_img}}--}}
-        {{--</div>--}}
-        {{--    <div>--}}
-        {{--    image devant :{{$productImg->front_img}}--}}
-        {{--</div>--}}
         <script>
             document.addEventListener('DOMContentLoaded', (e) => {
                 const select = document.getElementById('selector');

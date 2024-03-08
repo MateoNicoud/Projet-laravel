0fa248b3673c4d2c63195e44f1b891aca802d593
Eddy <eddy.vernet@le-campus-numerique.fr>
2024-03-08 16:12:29 +0100

    Bordel résolu

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+use App\Models\Product;
+use App\Models\ProductOption;
+
+
+class ProductController extends Controller
+{
+
+    public function index(string $slug)
+    {
+
+        $id = Category::where('slug', '=', $slug)->get();
+        $categoryProducts = Product::with('productOptions:id,price_ttc,product_id')->where('category_id', '=', $id[0]['id'])->get();
+        $categoryName = Category::where('slug', '=', $slug)->get();
+
+
+
+        foreach ($categoryProducts as $product) {
+                $minimumPrices[$product->id] = $product->productOptions->min('price_ttc');
+        }
+
+
+        return view('catalog', [
+            'categoryProducts' => $categoryProducts,
+            'categoryName' => $categoryName[0],
+            'minimumPrices' => $minimumPrices,
+        ]);
+    }
+
+    public
+    function show(string $id): view
+    {
+
+        // $idtest = 9b7fddae-bb74-4b84-b226-f3f69cd4f454
+        $product = Product::find($id);
+        $productOptions = (empty($product->productOptions)) ? 'null' : $product->productOptions;
+        $productCategory = (empty($product->category)) ? 'null' : $product->category;
+        $productComments = (empty($product->comments)) ? 'null' : $product->comments;
+        $productImg = (empty($productOptions[0]->url_img)) ? 'null' : $productOptions[0]->url_img;
+
+
+
+
+        return view('Product.show', [
+            'product' => $product,
+            'productOptions' => $productOptions,
+            'productCategory' => $productCategory,
+            'productComments' => $productComments,
+            'productImg' => $productImg
+
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AddProductController;
+use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/products/{categoryId}', [ProductController::class,'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomePageController::class, 'index']);
+
+Route::get('products/{id}',[ProductController::class,'show']);
+
+Route::post('addToCart/{id}', [AddProductController::class, 'addCart'])->name('addToCart');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

5a62afb84217d7f020812ce902fa7bab17571643
DUMAS <geremydumas@gmail.com>
2024-03-13 10:44:34 +0100

    #137 add feature gates for checking permission on dashboard

--- resources/views/dashboardperso.blade.php
+++ resources/views/dashboardperso.blade.php
@@ -5,15 +5,19 @@
         <div class="flex justify-between items-center">
             <div class="flex items-center">
                 <h1 class="text-lg mr-6">Dashboard</h1>
-                <div>
-                    <a href="{{route('addStorage')}}" class="mx-4 px-4">Ajouter produit</a>
-                    <a href="#" class="mx-4 px-4">Modifier produit</a>
-                    <a href="{{route('updateCategory')}}" class="mx-4 px-4">Modifier une catégorie</a>
-                    <a href="#" class="mx-4 px-4">Gestion des utilisateurs</a>
-                </div>
+                @if($user->credential->type_permission == 'admin')
+                    <div>
+                        <a href="{{route('addStorage')}}" class="mx-4 px-4">Ajouter produit</a>
+                        <a href="#" class="mx-4 px-4">Modifier produit</a>
+                        <a href="{{route('updateCategory')}}" class="mx-4 px-4">Modifier une catégorie</a>
+                        <a href="#" class="mx-4 px-4">Gestion des utilisateurs</a>
+                    </div>
+                @endif
+
             </div>
             <div class="flex">
                 <form method="post" action="{{route('logout')}}">
+                    @csrf
                     <input class="mx-6 px-6 hover:cursor-pointer" type="submit" value="Déconnexion">
                 </form>
                 <h3>{{$user->name}}</h3>
